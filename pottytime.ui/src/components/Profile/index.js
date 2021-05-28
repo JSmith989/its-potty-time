80c6033015f1a0fb62312cfb621461c2d9854ff0
Jordan Smith <jordansmith989@gmail.com>
2021-05-27 23:46:26 -0500

    profile almost finished

---
+++ pottytime.ui/src/components/Profile/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function index() {
+  return (
+    <div className="yourProfile">
+    <div className="card">
+        <div className="second d-flex flex-row">
+            <div> <img className="image" src="https://lh3.googleusercontent.com/a-/AOh14GgMbMP3HIo-7hnh4cOoP-xJyjcyR5DfB_a6L8Kf=s96-c" /> </div>
+                <div className="about d-flex flex-column"> <h2 className="yourName">Your Name</h2>
+                <div className="description"> <p>info about the stuff is going to go here once i get the stuff going on this stuff</p></div>
+                </div>
+        </div>
+    </div>
+</div>
+  );
+}
