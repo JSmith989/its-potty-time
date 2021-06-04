f06a14fb309bffc0bc82ffece248e4712200a5ab
Jordan Smith <jordansmith989@gmail.com>
2021-06-03 21:12:48 -0500

    file upload!

--- pottytime.ui/src/components/Forms/EditUserForm.js
+++ pottytime.ui/src/components/Forms/EditUserForm.js
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
+import firebase from 'firebase/app';
+import 'firebase/storage';
 import { useForm } from 'react-hook-form';
 import { updateUser } from '../../helpers/data/userData';
 
@@ -11,18 +14,25 @@ export default function EditUserForm({ user }) {
   } = useForm();
 
   const onSubmit = (data) => {
-    const userId = user.id;
-    const parsedId = Number(user.id);
-    const dataObject = {
-      id: parsedId,
-      firstName: data.firstName,
-      lastName: data.lastName,
-      email: data.email,
-      imageUrl: data.imageUrl,
-      description: data.description
-    };
-    updateUser(userId, dataObject)
-      .catch((err) => console.warn('nope', err));
+    const file = data.imageUrl[0];
+    const storageRef = firebase.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    fileRef.put(file).then((snapshot) => {
+      snapshot.ref.getDownloadURL().then((image) => {
+        const userId = user.id;
+        const parsedId = Number(user.id);
+        const dataObject = {
+          id: parsedId,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          email: data.email,
+          imageUrl: image,
+          description: data.description
+        };
+        updateUser(userId, dataObject)
+          .catch((err) => console.warn('nope', err));
+      });
+    });
   };
 
   return (
@@ -33,8 +43,8 @@ export default function EditUserForm({ user }) {
       <input defaultValue={user.lastName} {...register('lastName', { required: true })} />
       <h5>Email</h5>
       <input defaultValue={user.email} {...register('email', { required: true })} />
-      <h5>Image Url</h5>
-      <input defaultValue={user.imageUrl} {...register('imageUrl', { required: true })} />
+      <h5>Image</h5>
+      <input type="file" {...register('imageUrl', { required: true })} />
       <h5>Description</h5>
       <input defaultValue={user.description} {...register('description', { required: true })} />
       {errors.firstName && <span>This field is required</span>}
