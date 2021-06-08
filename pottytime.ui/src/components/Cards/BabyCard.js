1aa057685aa77aa402b58b497773739b627efb34
Jordan Smith <jordansmith989@gmail.com>
2021-06-07 20:40:11 -0500

    update baby

--- pottytime.ui/src/components/Cards/BabyCard.js
+++ pottytime.ui/src/components/Cards/BabyCard.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle
 } from 'reactstrap';
 import { babyPooped } from '../../helpers/data/activitydata';
+import Modal from '../Modal';
+import EditBaby from '../Forms/EditBaby';
+import getUid from '../../helpers/data/authData';
+import { getUserById } from '../../helpers/data/userData';
 
 export default function BabyCard({ baby }) {
+  const [user, setUser] = useState([]);
   const date = new Date(baby.birthday);
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,6 +23,19 @@ export default function BabyCard({ baby }) {
     });
     // then take to a success message or popup
   };
+
+  const getUser = (userId) => {
+    getUserById(userId).then((response) => {
+      setUser(response);
+    }).catch((err) => console.warn('error', err));
+  };
+
+  useEffect(() => {
+    const userId = getUid();
+    if (userId) {
+      getUser(userId);
+    }
+  }, [user]);
   return (
         <div className="babyContainer">
       <Card className="babyContent">
@@ -26,6 +44,13 @@ export default function BabyCard({ baby }) {
           </form>
         <CardImg top width="100%" className="babyImage" src={baby.imageUrl} alt="Card image cap" />
         <CardBody>
+          <Modal title={'Edit'} btnStyle={'cool-button'} buttonLabel={'Edit'}>
+            <EditBaby
+            baby={baby}
+            key={baby.id}
+            user={user}
+            />
+          </Modal>
           <CardTitle tag="h5">{baby.firstName} {baby.lastName}</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">Age: {baby.age}</CardSubtitle>
           <CardText>Birthday: {date.toDateString()}</CardText>
