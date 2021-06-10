992bb0d116c0904ec85ecc4d00f953ef6e5d9647
Jordan Smith <jordansmith989@gmail.com>
2021-06-10 12:28:01 -0500

    very stuck at the moment

--- pottytime.ui/src/views/Statistics.js
+++ pottytime.ui/src/views/Statistics.js
@@ -1,48 +1,94 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
+import PropTypes from 'prop-types';
+import { getBabyActivities } from '../helpers/data/activitydata';
 
-const Statisctics = () => {
-  const data = {
-    labels: ['Meats', 'Vegetables', 'Fruits'],
-    datasets: [
-      {
-        label: '# of food items',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
+export default function Stats(props) {
+  const [activities, setActivities] = useState([]);
+  const [data, setData] = useState({});
+  const [vegetables, setVeggies] = useState({});
+  const [fruits, setFruits] = useState({});
+  const [meats, setMeats] = useState({});
+
+  const getActivities = (babyId) => {
+    getBabyActivities(babyId).then((response) => {
+      setActivities(response);
+    });
   };
 
-  const options = {
-    scales: {
-      yAxes: [
+  useEffect(() => {
+    const babyId = props.match.params.id;
+    if (babyId) {
+      getActivities(babyId);
+    }
+  }, [activities]);
+
+  const dataChart = () => {
+    const meat = meats;
+    const veg = vegetables;
+    const fruit = fruits;
+    console.log(meat.length);
+    activities.forEach((activity) => {
+      if (activity.mealType === 0) {
+        setVeggies(activity);
+      }
+      if (activity.mealType === 1) {
+        setFruits(activity);
+      }
+      if (activity.mealType === 3) {
+        setMeats(activity);
+      }
+    });
+    setData({
+      labels: ['Meats', 'Vegetables', 'Fruits'],
+      datasets: [
         {
-          ticks: {
-            beginAtZero: true,
-          },
+          label: '# of food items',
+          data: [meat.length, veg.length, fruit.length],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
         },
       ],
-    },
+    });
   };
 
+  useEffect(() => {
+    dataChart();
+  }, [data]);
   return (
   <>
     <div className='header'>
       <h1 className='title'>Balanced Diet</h1>
     </div>
-    <Bar data={data} options={options} />
+    <Bar data={data} options={{
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    }} />
   </>
   );
-};
+}
 
-export default Statisctics;
+Stats.propTypes = {
+  user: PropTypes.any,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.any
+    })
+  }),
+};
