0dee681b99b04bc43caf6c26b6936eca68ad6beb
Jordan Smith <jordansmith989@gmail.com>
2021-06-10 23:02:28 -0500

    bar graph is working!

--- pottytime.ui/src/views/Statistics.js
+++ pottytime.ui/src/views/Statistics.js
@@ -1,50 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import { Bar } from 'react-chartjs-2';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Bar } from 'react-chartjs-2';
 import { getBabyActivities } from '../helpers/data/activitydata';
 
-export default function Stats(props) {
-  const [activities, setActivities] = useState([]);
-  const [data, setData] = useState({});
-  const [vegetables, setVeggies] = useState({});
-  const [fruits, setFruits] = useState({});
-  const [meats, setMeats] = useState({});
+export default class Stats extends Component {
+  state = {
+    activities: [],
+  }
 
-  const getActivities = (babyId) => {
-    getBabyActivities(babyId).then((response) => {
-      setActivities(response);
-    });
-  };
+  componentDidMount() {
+    const babyId = this.props.match.params.id;
+    this.getActivities(babyId);
+  }
 
-  useEffect(() => {
-    const babyId = props.match.params.id;
-    if (babyId) {
-      getActivities(babyId);
-    }
-  }, [activities]);
+   getActivities = (babyId) => {
+     getBabyActivities(babyId).then((response) => {
+       this.setState({ activities: response });
+     });
+   };
 
-  const dataChart = () => {
-    const meat = meats;
-    const veg = vegetables;
-    const fruit = fruits;
-    console.log(meat.length);
-    activities.forEach((activity) => {
-      if (activity.mealType === 0) {
-        setVeggies(activity);
-      }
-      if (activity.mealType === 1) {
-        setFruits(activity);
-      }
-      if (activity.mealType === 3) {
-        setMeats(activity);
-      }
-    });
-    setData({
+   render() {
+     const {
+       activities,
+     } = this.state;
+
+     const veg = () => {
+       const v = [];
+       activities.forEach((activity) => {
+         if (activity.mealType === 0) {
+           v.push(activity);
+         }
+       });
+       return v;
+     };
+     const fruit = () => {
+       const f = [];
+       activities.forEach((activity) => {
+         if (activity.mealType === 1) {
+           f.push(activity);
+         }
+       });
+       return f;
+     };
+     const meat = () => {
+       const m = [];
+       activities.forEach((activity) => {
+         if (activity.mealType === 2) {
+           m.push(activity);
+         }
+       });
+       return m;
+     };
+     return (
+      <>
+     <div className='header'>
+      <h1 className='title'>Balanced Diet</h1>
+    </div>
+    <Bar data={{
       labels: ['Meats', 'Vegetables', 'Fruits'],
       datasets: [
         {
           label: '# of food items',
-          data: [meat.length, veg.length, fruit.length],
+          data: [meat().length, veg().length, fruit().length],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(75, 192, 192, 0.2)',
@@ -58,30 +75,18 @@ export default function Stats(props) {
           borderWidth: 1,
         },
       ],
-    });
-  };
-
-  useEffect(() => {
-    dataChart();
-  }, [data]);
-  return (
-  <>
-    <div className='header'>
-      <h1 className='title'>Balanced Diet</h1>
-    </div>
-    <Bar data={data} options={{
+    }} options={{
       scales: {
         yAxes: [
           {
-            ticks: {
-              beginAtZero: true,
-            },
+            ticks: { beginAtZero: true },
           },
         ],
       },
     }} />
-  </>
-  );
+   </>
+     );
+   }
 }
 
 Stats.propTypes = {
